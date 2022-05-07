d3c7bbce507f199be477ed87b79aacd8ab3d80ee
Oliver Giles <ogiles@email.com>
2022-05-07 15:11:24 +0100

    finished drawing grid

--- main.cpp
+++ main.cpp
@@ -18,7 +18,28 @@ void Setup()
 }
 void Draw()
 {
+  system("clear");
+  for (int i = 0; i < width + 2; i++)
+    cout << "#";
+  cout << endl;
 
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      if (j == 0)
+        cout << "#";
+      else
+        cout << " ";
+
+      if (j == width - 1)
+        cout << "#";
+    }
+    cout << endl;
+  }
+  for (int i = 0; i < width + 2; i++)
+    cout << "#";
+  cout << endl;
 }
 void Input()
 {
