d21d89dbf9c3640f6814580879f6bceb3aa3c754
Oliver Giles <ogiles@email.com>
2022-05-07 15:03:29 +0100

    setup complete

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+void Setup()
+{
+  gameOver = false;
+  dir = STOP;
+  x = width / 2;
+  y = height / 2;
+  fruitX = rand() % width;
+  fruitY = rand() % height;
+  score = 0;
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+int main()
+{
+  Setup();
+  while (!gameOver)
+  {
+    Draw();
+    Input();
+    Logic();
+  }
+}
